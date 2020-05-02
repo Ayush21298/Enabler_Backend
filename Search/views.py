4e9282692f53fa6f825bf958051b1403d2540a29
Ayush21298 <patel.ayush08@gmail.com>
2020-05-02 19:52:15 +0530

    Added Search Box

--- Search/views.py
+++ Search/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, FileResponse
+from django.urls import reverse
+from django.template import loader
+from django.core.files.storage import FileSystemStorage
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view, parser_classes, authentication_classes
+from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser, MultiPartParser
+
+import json
+import requests
+import datetime
+import base64
+import hashlib
+
+from serializers import *
+from utils import *
+
+from urllib.parse import unquote
+
+@api_view(['GET'])
+def all(request):
+	if request.method == 'GET':
+
+		return Response({})
+
