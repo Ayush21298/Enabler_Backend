0e8415431ebc5e25a3d18d12f927d4e3985258fe
Ayush21298 <patel.ayush08@gmail.com>
2020-08-02 14:47:55 +0530

    Added Video Model

--- Search/models.py
+++ Search/models.py
@@ -7,12 +7,11 @@ from utils import *
 class Video(models.Model):
 	url = models.CharField(max_length=128, null=True)
 	file = models.FileField(upload_to=get_upload_path_video, blank=True, null=True)
-	title = models.CharField(max_length=64, null=True)
+	title = models.CharField(max_length=64)
 	description = models.TextField(max_length=64, null=True)
 	author = models.CharField(max_length=64, null=True)
-	uploader = models.CharField(max_length=64, null=True)
-	approved = models.BooleanField(default=False)
+	tag = models.CharField(max_length=1024, null=True)
 	timestamp = models.DateTimeField(default=timezone.now, null=True)
 
 	def __str__(self):
-		return self.url
+		return self.title
--- utils.py
+++ utils.py
@@ -2,4 +2,4 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def get_upload_path_video(instance, filename):
-	return 'video/{0}/{1}/{2}/{3}'.format(instance.timestamp, instance.username, instance.title, filename)
+	return 'static/video/{0}/{1}'.format(instance.timestamp, filename)
